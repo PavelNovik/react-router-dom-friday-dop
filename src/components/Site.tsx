68b60965927601e544722386dd4f1ce3500917a9
Pavel Novik <brn2befree@gmail.com>
2023-10-13 20:24:18 +0300

    Finish friday dop about react-router-dom v6

---
+++ src/components/Site.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './Site.module.css'
+import {PageOne} from "./pages/PageOne";
+import {PageTwo} from "./pages/PageTwo";
+import {PageThree} from "./pages/PageThree";
+import {Link, Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./pages/Error404";
+import {S} from './pages/__styles'
+import Page from "./pages/Page";
+import {dataState} from "../data/dataState";
+
+
+export const Site = () => {
+        return (
+            <div>
+                <div className={styles.header}><h1>HEADER</h1></div>
+                <div className={styles.body}>
+                    <div className={styles.nav}>
+                        {/*<div><Link  to={'/page1'}>Page1</Link></div>*/}
+                        {/*<div><NavLink className={({isActive})=>isActive? styles.active: styles.navLink}  to={'/page1'}>Page1</NavLink></div>*/}
+                        <S.NavWrapper><NavLink to={'/page/0'}>Page1</NavLink></S.NavWrapper>
+                        <S.NavWrapper><NavLink to={'/page/1'}>Page2</NavLink></S.NavWrapper>
+                        <S.NavWrapper><NavLink to={'/page/2'}>Page3</NavLink></S.NavWrapper>
+                    </div>
+                    <div className={styles.content}>
+                        <Routes>
+                            <Route path={'/'} element={<Navigate to={'/page/0'}/>}/>
+                            {/*<Route path={'/page1'} element={<PageOne/>}/>*/}
+                            <Route path={'/page/:id'} element={<Page pages={dataState.pages}/>}/>
+                            <Route path={'/*'} element={<Error404/>}/>
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+;
+
---
+++ src/components/pages/Page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {PagesType} from "../../data/dataState";
+import {useLocation, useParams} from "react-router-dom";
+import {Error404} from "./Error404";
+
+type PropsType = {
+    pages: PagesType[]
+}
+const Page: React.FC<PropsType> = ({pages}) => {
+    const params = useParams()
+    const location = useLocation()
+    // console.log(location)
+    return (
+        <div>
+            {location.pathname==='/page/0' && <div>SECRET TEXT</div>}
+            {pages[Number(params.id)] ? <div>
+                {pages[Number(params.id)].heading}
+                {pages[Number(params.id)].about}
+            </div> : <Error404/>}
+
+        </div>
+    );
+};
+
+export default Page;
